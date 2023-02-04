b3a263dddc696d84e42d8294b7e249ca08d68917
orbayemre <yeoorbay@gmail.com>
2023-02-04 11:37:19 +0300

    Responsive Design

--- src/script.js
+++ src/script.js
@@ -1,10 +1,9 @@
+
+//Controlling menu items while scrolling
 const sections = document.querySelectorAll("section");
 const menuItems = document.querySelectorAll(".menu a");
-
 window.addEventListener("scroll", ()=>{
-
     let scrollY = window.pageYOffset;
-
     let activePage = "main";
 
     let main= sections[0].offsetHeight-100;
@@ -17,15 +16,15 @@ window.addEventListener("scroll", ()=>{
     else if(scrollY < main+portfolio+about) activePage = "about";
     else if(scrollY <  main+portfolio+about+contact) activePage = "contact";
 
-
     menuItems.forEach(item =>{
         if(item.id === activePage) item.className = "active";
         else item.className = "";
     })
-
 });
 
+//Menu items click events with jQuery
 $(document).ready(function(){
+
 
     $("#portfolio").click(function() {
         $('html, body').animate({
@@ -36,19 +35,28 @@ $(document).ready(function(){
 
     $("#about").click(function() {
         $('html, body') .animate({
-            scrollTop: $(".about").offset().top-80
+            scrollTop: $(".about").offset().top-66
         }, 500);
         return false;
     });
 
     $("#contact").click(function() {
         $(' html,body') .animate({
-            scrollTop: $(".contact").offset().top
+            scrollTop: $(".contact").offset().top-66
         }, 500);
         return false;
     });
-
 });
 
-
-
+//Back to top button
+const backToTop = document.querySelector(".backToTop");
+window.onscroll = ()=> {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        backToTop.style.display = "flex";
+    } else {
+        backToTop.style.display = "none";
+    }
+};
+backToTop.addEventListener("click",()=>{
+    window.scrollTo({top:0,left:0,behavior:"smooth"})
+});
